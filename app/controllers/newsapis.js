6e0b1bfd58b587aa063e92c74300291a477a599b
vivek Kumar <vivek@flashbox.in>
2016-08-02 13:22:08 +0530

    Update different apis

--- app/controllers/newsapis.js
+++ app/controllers/newsapis.js
@@ -30,3 +30,68 @@ router.get('/slider', function (req, res) {
     });
 
 })
+router.get('/GetCategoryList', function (req, res) {
+
+    var options = {
+        method: 'POST',
+        url: 'http://indianonlinenews.com/Default.aspx/GetCategoryList',
+        body:'',
+        headers: {
+            accept: 'application/json',
+            'content-type':'application/json'
+        }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.send(body);
+    });
+
+})
+
+router.get('/GetStatetNews/:stateId/:count', function (req, res) {
+    var postData = {
+        SID: req.params.stateId,
+        Count: req.params.count
+    }
+    console.log(postData);
+
+    var options = {
+        method: 'POST',
+        url: 'http://indianonlinenews.com/Default.aspx/GetStatetNews',
+        body:JSON.stringify(postData),
+        headers: {
+            accept: 'application/json',
+            'content-type':'application/json'
+        }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.send(body);
+    });
+
+})
+
+
+router.get('/GetNewsByCategory/:ncid/:count', function (req, res) {
+
+    var postData = {
+        NCID: req.params.ncid,
+        Count: req.params.count
+    }
+    console.log(postData);
+
+    var options = {
+        method: 'POST',
+        url: 'http://indianonlinenews.com/Default.aspx/GetNewsByCategory',
+        body:JSON.stringify(postData),
+        headers: {
+            accept: 'application/json',
+            'content-type':'application/json'
+        }
+    };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.send(body);
+    });
+
+})
