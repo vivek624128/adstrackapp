2130cf283201d6a5094f830e946614e69b67ca9d
vivek Kumar <vivek@flashbox.in>
2017-04-03 11:58:01 +0530

    Update adsTrackapp apis

--- public/js/service/apiService.js
+++ public/js/service/apiService.js
@@ -32,7 +32,7 @@ NEC.provider('apiPath', function () {
     return {
         $get: function () {
             return {
-                baseUrl: 'http://localhost:8080/',
+                // baseUrl: 'http://localhost:8080/'
 
                baseUrl: 'http://newsapp-myflash.rhcloud.com/'
             }
