d5c33a424faa952fba0cff03178936f9b8c938e8
vivek Kumar <vivek@flashbox.in>
2017-04-05 18:08:54 +0530

    remove mail service

--- app/controllers/userCredential.js
+++ app/controllers/userCredential.js
@@ -75,8 +75,9 @@ router.get('/sendMail', function (req, res) {
 router.post('/login/authenticate', function (req, res) {
     console.log(req.body)
     var permission =req.body.permission;
-    users.find({username:req.body.userId, password:md5(req.body.password)},{username:1, permission:1}, function (err, data) {
+    users.find({username:req.body.userId, password:md5(req.body.password)},{_id:1,username:1, permission:1, campaign:1}, function (err, data) {
         if (err) throw err;
+        console.log(data)
         if(data.length>0){
             if(data[0].permission == permission){
                 res.send(  responseMsg.response('200','Success', data));
