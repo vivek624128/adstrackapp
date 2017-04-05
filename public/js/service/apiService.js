fc69e02ba89951feaecdb7256b146f4a486cf5a3
Vivek Kumar <vivek@flashbox.in>
2017-04-05 21:05:32 +0530

    Update server base url

--- public/js/service/apiService.js
+++ public/js/service/apiService.js
@@ -83,9 +83,9 @@ NEC.provider('apiPath', function () {
     return {
         $get: function () {
             return {
-                baseUrl: 'http://localhost:8080/'
+                // baseUrl: 'http://localhost:8080/'
 
-               // baseUrl: 'http://newsapp-myflash.rhcloud.com/'
+               baseUrl: 'http://newsapp-myflash.rhcloud.com/'
             }
         }
     }
