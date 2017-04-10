0fbb53326cbf8c59149bfdce3fe24a4ca62186b6
vivek Kumar <vivek@flashbox.in>
2017-04-10 09:34:26 +0530

    update timezone

--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@ var path = require('path'),
     env = process.env.NODE_ENV || 'development';
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+process.env.TZ = 'Asia/Kolkata';
 var config = {
   development: {
     root: rootPath,
