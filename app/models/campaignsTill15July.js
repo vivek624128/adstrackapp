fcaf5cb07e9615b77b60b69d7b7f983e037c92bd
Vivek Kumar <viveksmart5@gmail.com>
2017-07-15 18:29:05 +0530

    checking feed issue

---
+++ app/models/campaignsTill15July.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+schema = mongoose.Schema;
+
+var campaigns1Schema = new schema({
+    campaignName: String,
+    startDate: Date,
+    endDate: Date,
+    projectId: [{type: String, ref: 'projects'}],
+    campaignType: [{type: String, ref: 'adsCategory'}],
+    creationDate: Date,
+    campaign: [
+        {
+            assignDate: Date,
+            user: [{type: String, ref: 'users'}],
+            vehicleId: [{type: String, ref: 'vehicles'}],
+            updates: [{
+                location: [{
+                    latitude: String,
+                    longitude: String,
+                    address: String,
+                    caption: String
+                }],
+                updatedOn: Date,
+                updateStatus: String
+            }]
+        }
+    ],
+    status: String
+})
+
+mongoose.model('campaigns1', campaigns1Schema);
