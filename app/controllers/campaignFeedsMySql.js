c20cdf9e4be8f413c6e9fe3e097babf59acc2dbe
Vivek Kumar <viveksmart5@gmail.com>
2017-08-07 17:08:49 +0530

    checking mysql db connection

--- app/controllers/campaignFeedsMySql.js
+++ app/controllers/campaignFeedsMySql.js
@@ -128,12 +128,19 @@ router.get('/campaignFeeds/:startDate/:endDate', function (req, res) {
         if (err) throw err;
         // console.log(result);
         res.send(result);
-        if(result){
-            // connection.end();
-        }
-
     });
 
+})
+router.get('/campaignFeeds/:startDate/:endDate/:vehicleId', function (req, res) {
+    var data = req.params;
+    var startDate = moment(data.startDate).startOf('day').format();
+    var endDate = moment(data.endDate).endOf('day').format();
+    console.log(startDate + ' ------- ' + endDate)
+    connection.query("SELECT * FROM `campaignFeeds` WHERE vehicleId = '"+vehicleId+"' AND `updatedOn` BETWEEN '" + startDate + "' AND '" + endDate + "' ORDER BY `updatedOn` DESC", function (err, result, fields) {
+        if (err) throw err;
+        // console.log(result);
+        res.send(result);
+    });
 })
 // connection.end();
 
