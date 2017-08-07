6ec573f7aefdb75d2a4519786e18e08431bdee8d
Vivek Kumar <viveksmart5@gmail.com>
2017-08-07 11:26:44 +0530

    checking mysql db connection

--- app/controllers/userMysql.js
+++ app/controllers/userMysql.js
@@ -14,14 +14,38 @@ module.exports = function (app) {
     app.use(cors());
 };
 
-router.post('/userTable', function (req, res) {
+var userSchema = ['userType VARCHAR(255)',
+    'username  VARCHAR(255)',
+    'password  VARCHAR(255)',
+    'dateTime  VARCHAR(255)',
+    'fullName  VARCHAR(255)',
+    'contactNo  VARCHAR(255)',
+    'emailId  VARCHAR(255)',
+    'Address  VARCHAR(255)',
+    'state  VARCHAR(255)',
+    'district  VARCHAR(255)',
+   'block  VARCHAR(255)',
+    'postalCode  VARCHAR(255)',
+    'aadharNo  VARCHAR(255)',
+    'panNo  VARCHAR(255)',
+    'profilePic  VARCHAR(255)',
+    'aadharCopy  VARCHAR(255)',
+    'advertiseId  VARCHAR(255)',
+    'driverLicense  VARCHAR(255)',
+    'driverLicenseDoc  VARCHAR(255)',
+    'permission  VARCHAR(255)']
+
+
+
+router.get('/userTable', function (req, res) {
     connection.connect(function (err) {
         if (err) throw err;
         console.log("Connected!");
-        var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+        var sql = "CREATE TABLE customers ("+userSchema+")";
         connection.query(sql, function (err, result) {
             if (err) throw err;
             console.log("Table created");
+            res.send("Table created")
         });
     });
 })
